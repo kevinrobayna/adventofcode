a0f8cfa38458493184e859442f34dad3a7ebcc9c
Kevin Robayna <me@kevinrobayna.com>
2022-11-30 23:01:32 +0000

    simplify solution

--- 2020/day-01/day01.kts
+++ 2020/day-01/day01.kts
@@ -1,71 +1,61 @@
-import java.util.stream.Collectors
 import java.io.File
-import kotlin.String
-import java.nio.file.Paths
+import java.util.stream.Collectors
 
 
-fun readFile(filename: String): List<String> {
-    val path = Paths.get(filename).toAbsolutePath().toString()
-    return File(path).useLines { it.toList() }
+fun readFile(fileName: String): String = File(fileName).readText(Charsets.UTF_8)
+
+fun parseProblem(content: String): List<Int> {
+    content.split("\n").stream().map { it.toInt() }.collect(Collectors.toList())
 }
 
-class Day1(
-        private val content: List<String>
-) {
-    fun solve(): Int {
-        val values = parseProblem()
-        val pairs: MutableList<Pair<Int, Int>> = mutableListOf()
+fun solve(content: String): Int {
+    val values = parseProblem(content)
+    val pairs: MutableList<Pair<Int, Int>> = mutableListOf()
 
-        for (outLoop in values.indices) {
-            for (innerLoop in values.indices) {
-                if (!outLoop.equals(innerLoop)) {
-                    val first = Math.max(values[outLoop], values[innerLoop])
-                    val second = Math.min(values[outLoop], values[innerLoop])
-                    pairs.add(Pair(first, second))
-                }
+    for (outLoop in values.indices) {
+        for (innerLoop in values.indices) {
+            if (!outLoop.equals(innerLoop)) {
+                val first = Math.max(values[outLoop], values[innerLoop])
+                val second = Math.min(values[outLoop], values[innerLoop])
+                pairs.add(Pair(first, second))
             }
         }
-
-        val pair = pairs
-                .filter { it.first + it.second == 2020 }
-                .distinct()
-                .get(0)
-
-        return pair.first * pair.second
     }
 
-    fun solve2(): Int {
-        val values = parseProblem()
-        val tuples: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+    val pair = pairs
+        .filter { it.first + it.second == 2020 }
+        .distinct()
+        .get(0)
 
-        for (outLoop in values) {
-            for (innerLoop in values) {
-                for (innerInnerLoop in values) {
-                    val points = listOf(outLoop, innerLoop, innerInnerLoop).sorted()
-                    tuples.add(Triple(points[0], points[1], points[2]))
-                }
+    return pair.first * pair.second
+}
+
+fun solve2(content: String): Int {
+    val values = parseProblem(content)
+    val tuples: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+
+    for (outLoop in values) {
+        for (innerLoop in values) {
+            for (innerInnerLoop in values) {
+                val points = listOf(outLoop, innerLoop, innerInnerLoop).sorted()
+                tuples.add(Triple(points[0], points[1], points[2]))
             }
         }
-
-        val triple = tuples
-                .filter { it.first.plus(it.second).plus(it.third) == 2020 }
-                .distinct()
-                .get(0)
-
-        return triple.first * triple.second * triple.third
     }
 
-    private fun parseProblem(): List<Int> {
-        return this.content.stream().map { it.toInt() }.collect(Collectors.toList())
-    }
+    val triple = tuples
+        .filter { it.first.plus(it.second).plus(it.third) == 2020 }
+        .distinct()
+        .get(0)
+
+    return triple.first * triple.second * triple.third
 }
 
 
-val test = readFile("2020/day-01/test.txt")
-val real = readFile("2020/day-01/input.txt")
+val test = readFile("test.txt")
+val real = readFile("input.txt")
 
-
-println("Part1 Test ${Day1(test).solve()}")
-println("Part1 ${Day1(real).solve()}")
-println("Part2 Test ${Day1(test).solve2()}")
-println("Part2 ${Day1(real).solve2()}")
+println("Part1 Test ${solve(test)}")
+println("Part1 ${solve(real)}")
+println("Part2 Test ${solve2(test)}")
+println("Part2 ${solve2(real)}")
