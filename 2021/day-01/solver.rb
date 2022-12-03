686d238e326446888f7e45dab8b826637932b1f6
Kevin Robayna <me@kevinrobayna.com>
2022-12-03 11:30:03 +0000

    Modernise 2021/day1

---
+++ 2021/day-01/solver.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+def compare_solutions(expected, actual)
+  raise "Expected #{expected} but got #{actual}" unless expected == actual
+end
+
+def read_file(*args)
+  File.read(File.join(File.absolute_path('.'), *args))
+end
+
+def solve(filename)
+  numbers = read_file(filename).split("\n").reject(&:empty?).map(&:to_i)
+  count_increases(numbers)
+end
+
+def solve2(filename)
+  numbers = read_file(filename).split("\n").reject(&:empty?).map(&:to_i)
+  aggregated_numbers = []
+  numbers.each_with_index do |value, index|
+    next if index < 2
+
+    aggregated_numbers.append(numbers[index - 2] + numbers[index - 1] + value)
+  end
+  count_increases(aggregated_numbers)
+end
+
+def count_increases(numbers)
+  counter = 0
+  numbers.each_with_index do |value, index|
+    next if index.eql? 0
+
+    counter += 1 if numbers[index - 1] < value
+  end
+  counter
+end
+
+compare_solutions(7, solve('2021/day-01/test.txt'))
+puts 'Part1', solve('2021/day-01/input.txt')
+
+compare_solutions(5, solve2('2021/day-01/test.txt'))
+puts 'Part2', solve2('2021/day-01/input.txt')
