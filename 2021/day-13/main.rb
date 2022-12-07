91eca6b14eb7aaca4026df44a91d1b7a58fb48e8
Kevin Robayna <me@kevinrobayna.com>
2022-12-07 18:42:50 +0000

    Batch create ruby files for every day

---
+++ 2021/day-13/main.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+def compare_solutions(expected, actual)
+  raise "Expected #{expected} but got #{actual}" unless expected == actual
+end
+
+def read_file(*args)
+  File.read(File.join(File.absolute_path('.'), *args))
+end
+
+def solve(filename)
+  read_file(filename)
+  0
+end
+
+def solve2(filename)
+  read_file(filename)
+  0
+end
+
+compare_solutions(0, solve('2021/day-13/test.txt'))
+puts 'Part1', solve('2021/day-13/input.txt')
+
+compare_solutions(0, solve2('2021/day-13/test.txt'))
+puts 'Part2', solve2('2021/day-13/input.txt')
