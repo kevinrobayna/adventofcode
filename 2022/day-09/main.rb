e7c2d2215a0ede8533efda99683d217d958cc4dc
Kevin Robayna <me@kevinrobayna.com>
2022-12-15 22:25:17 +0000

    Clean up solution for day9

--- 2022/day-09/main.rb
+++ 2022/day-09/main.rb
@@ -17,6 +17,50 @@ MOVEMENTS = {
   'D' => [0, -1]
 }.freeze
 
+class Knot
+  attr_reader :pos_x, :pos_y
+
+  def initialize
+    @pos_x = 0
+    @pos_y = 0
+    @memory = Set.new([0, 0])
+  end
+
+  def move(direction)
+    next_x, next_y = MOVEMENTS[direction]
+    @pos_x += next_x
+    @pos_y += next_y
+
+    @memory.add([@pos_x, @pos_y])
+  end
+
+  # Move tail to follow head
+  # Since we are moving each position at the time, one node can be:
+  #   0 steps away -> no movement
+  #   1 or 2 steps away (ahead) -> move towards on the same axis (x++ or y++)
+  #   -1 or -2 steps away (behind) -> move behind on the same axis (x-- or y--)
+  def follow(other)
+    return unless nearby?(other)
+
+    move_x = other.pos_x - @pos_x
+    move_y = other.pos_y - @pos_y
+    @pos_x += move_x.abs == 2 ? move_x / 2 : move_x
+    @pos_y += move_y.abs == 2 ? move_y / 2 : move_y
+
+    @memory.add([@pos_x, @pos_y])
+  end
+
+  def steps
+    @memory.size
+  end
+
+  private
+
+  def nearby?(other)
+    [(other.pos_x - @pos_x).abs, (other.pos_y - @pos_y).abs].max > 1
+  end
+end
+
 def solve(filename)
   calculate_tail_positions(filename, 2)
 end
@@ -25,54 +69,22 @@ def solve2(filename)
   calculate_tail_positions(filename, 10)
 end
 
-def calculate_tail_positions(filename, knots)
-  positions = knots.times.map do
-    [0, 0]
-  end
-  tail_positions = Set[positions.last]
+def calculate_tail_positions(filename, knot_count)
+  knots = knot_count.times.map { Knot.new }
 
   read_file(filename).each_line.map do |line|
     direction, movement = line.split(' ')
     movement.to_i.times do
-      positions.each.with_index do |_, inx|
-        positions[inx] = if inx.zero?
-                           head = positions[inx]
-                           move(direction, head)
-                         else
-                           head = positions[inx - 1]
-                           tail = positions[inx]
-                           follow(head, tail)
-                         end
+      knots.each.with_index do |knot, inx|
+        if inx.zero?
+          knot.move(direction)
+        else
+          knot.follow(knots[inx - 1])
+        end
       end
-      tail_positions.add(positions.last)
     end
   end
-  tail_positions.size
-end
-
-def move(direction, knot)
-  next_x, next_y = MOVEMENTS[direction]
-  knot_x, knot_y = knot
-  [knot_x + next_x, knot_y + next_y]
-end
-
-def follow(head, tail)
-  head_x, head_y = head
-  tail_x, tail_y = tail
-  distance = [(head_x - tail_x).abs, (head_y - tail_y).abs].max
-
-  # Move tail to follow head
-  # Since we are moving each position at the time, one node can be:
-  #   0 steps away -> no movement
-  #   1 or 2 steps away (ahead) -> move towards on the same axis (x++ or y++)
-  #   -1 or -2 steps away (behind) -> move behind on the same axis (x-- or y--)
-  return tail if distance <= 1
-
-  move_x = head_x - tail_x
-  move_y = head_y - tail_y
-  next_x = move_x.abs == 2 ? move_x / 2 : move_x
-  next_y = move_y.abs == 2 ? move_y / 2 : move_y
-  [tail_x + next_x, tail_y + next_y]
+  knots.last.steps
 end
 
 compare_solutions(13, solve('2022/day-09/test.txt'))
