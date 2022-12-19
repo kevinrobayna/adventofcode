4e109539fec70b56f976115765b4c8e594791842
Kevin Robayna <me@kevinrobayna.com>
2022-12-19 18:34:23 +0000

    Day 13

--- 2022/day-13/main.rb
+++ 2022/day-13/main.rb
@@ -10,18 +10,94 @@ def read_file(*args)
   File.read(File.join(File.absolute_path('.'), *args))
 end
 
+def parse_file(filename)
+  read_file(filename).each_line.reject do |line|
+    line.strip.empty?
+  end.map do |line|
+    eval(line.strip)
+  end
+end
+
 def solve(filename)
-  read_file(filename)
+  signals = parse_file(filename)
+
+  pair_inx = 1
+  index_sum = []
+  (0...signals.length).step(2).each do |signal|
+    left = signals[signal]
+    right = signals[signal + 1]
+    puts "\n== Pair #{pair_inx} =="
+    result = compare(left, right)
+    index_sum << pair_inx if result.positive?
+    pair_inx += 1
+  end
+
+  puts "\n== Results == [#{index_sum.join(', ')}]"
+  index_sum.sum
+end
+
+def compare(left, right)
+  puts "- Compare #{left} vs #{right}"
+  if left.instance_of?(Integer) && right.instance_of?(Integer)
+    if left < right
+      puts '- Left side is smaller, so inputs are in the right order'
+      return 1
+    elsif left > right
+      puts '- Right side is smaller, so inputs are not in the right order'
+      return -1
+    end
+  elsif left.instance_of?(Array) && right.instance_of?(Array)
+    (0...left.length).each do |i|
+      break if left[i].nil? || right[i].nil?
+
+      result = compare(left[i], right[i])
+      return result if result.positive? || result.negative?
+    end
+    if left.length < right.length
+      puts '- Left side ran out of items, so inputs are in the right order'
+      return 1
+    elsif left.length > right.length
+      puts '- Right side ran out of items, so inputs are not in the right order'
+      return -1
+    end
+  elsif left.instance_of?(Array) || right.instance_of?(Array)
+    if left.instance_of?(Array)
+      if right.nil?
+        puts '- Right side ran out of items, so inputs are not in the right order'
+        return -1
+      end
+      puts "- Mixed types; convert right to [#{right}] and retry comparison"
+      result = compare(left, [right])
+    else
+      if left.nil?
+        puts '- Left side ran out of items, so inputs are in the right order'
+        return 1
+      end
+      puts "- Mixed types; convert left to [#{left}] and retry comparison"
+      result = compare([left], right)
+    end
+    return result if result.positive? || result.negative?
+  end
   0
 end
 
 def solve2(filename)
-  read_file(filename)
-  0
+  signals = parse_file(filename)
+  signals.push([[2]], [[6]])
+
+  sorted = signals.sort do |a, b|
+    result = compare(a, b)
+    result.positive? ? -1 : 1
+  end
+
+  div_one = sorted.find_index([[2]]) + 1
+  div_two = sorted.find_index([[6]]) + 1
+
+  div_one * div_two
 end
 
-compare_solutions(0, solve('2022/day-13/test.txt'))
+compare_solutions(13, solve('2022/day-13/test.txt'))
 puts 'Part1', solve('2022/day-13/input.txt')
 
-compare_solutions(0, solve2('2022/day-13/test.txt'))
+compare_solutions(140, solve2('2022/day-13/test.txt'))
 puts 'Part2', solve2('2022/day-13/input.txt')
