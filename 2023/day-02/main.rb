24ae7817e0b0cd64552ea7d3427e0f6f9e301555
Kevin Robayna <me@kevinrobayna.com>
2023-12-02 16:21:28 +0000

    Add day2
    Oh my part 2 was veeery confusing

---
+++ 2023/day-02/main.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'pry'
+def compare_solutions(expected, actual)
+  raise "Expected #{expected} but got #{actual}" unless expected == actual
+
+  puts "Congratulations! Got expected result (#{expected})"
+end
+
+def read_file(filename)
+  # Get the directory of the currently executing script
+  # Join the script directory with the filename
+  # Read the content of the file
+  File.read(File.join(__dir__, filename))
+end
+
+def parse_file(filename)
+  read_file(filename).each_line.map do |line|
+    game_id, games = line.split(':')
+    cubes = games.strip.split(';').map do |sets|
+      sets.split(',').map(&:split).to_h do |x|
+        [x[1].to_sym, x[0].to_i]
+      end
+    end
+
+    [game_id.split[1].to_i, cubes]
+  end.to_h
+end
+
+def solve(filename)
+  game = parse_file(filename)
+
+  limits = { red: 12, green: 13, blue: 14 }
+  possible_games = game.select do |_game_id, sets|
+    sets.all? do |x|
+      (x[:red] || 0) <= limits[:red] &&
+        (x[:green] || 0) <= limits[:green] &&
+        (x[:blue] || 0) <= limits[:blue]
+    end
+  end
+  possible_games.sum { |game_id, _| game_id }
+end
+
+def solve2(filename)
+  parse_file(filename).map do |_id, sets|
+    max = { red: 0, green: 0, blue: 0 }
+    sets.each do |x|
+      max[:red] = [x[:red] || 0, max[:red]].max
+      max[:green] = [x[:green] || 0, max[:green]].max
+      max[:blue] = [x[:blue] || 0, max[:blue]].max
+    end
+
+    max
+  end.sum do |set|
+    set[:red] * set[:blue] * set[:green]
+  end
+end
+
+compare_solutions(8, solve('test.txt'))
+puts 'Part1', solve('input.txt')
+
+compare_solutions(2286, solve2('test.txt'))
+puts 'Part2', solve2('input.txt')
