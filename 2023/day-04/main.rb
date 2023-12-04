c3b659b75b05cac0ada546482c770dd0d2d534f1
Kevin Robayna <me@kevinrobayna.com>
2023-12-04 13:54:08 +0000

    publish solution of day 4
    slowly but surely

--- 2023/day-04/main.rb
+++ 2023/day-04/main.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry'
 def compare_solutions(expected, actual)
   raise "Expected #{expected} but got #{actual}" unless expected == actual
 
@@ -13,18 +14,45 @@ def read_file(filename)
   File.read(File.join(__dir__, filename))
 end
 
+def parse_problem(filename)
+  read_file(filename).each_line.map do |line|
+    _, numbers = line.split(':')
+    left, right = numbers.split('|')
+    left.strip.split(' ').sort & right.strip.split(' ').sort
+  end
+end
+
 def solve(filename)
-  read_file(filename)
-  0
+  parse_problem(filename).reduce(0) do |acc, numbers|
+    acc + if numbers.empty?
+            0
+          elsif numbers.length == 1
+            1
+          else
+            result = 1
+            (numbers.length - 1).times { result *= 2 }
+
+            result
+          end
+  end
 end
 
 def solve2(filename)
-  read_file(filename)
-  0
+  scratchcards = []
+  parse_problem(filename).each_with_index do |numbers, inx|
+    current = inx + 1
+    scratchcards << current
+    state = scratchcards.tally
+    (1..numbers.length).each do |x|
+      state[current].times { scratchcards << current + x }
+    end
+  end
+
+  scratchcards.tally.values.sum
 end
 
-compare_solutions(0, solve('test.txt'))
+compare_solutions(13, solve('test.txt'))
 puts 'Part1', solve('input.txt')
 
-compare_solutions(0, solve2('test.txt'))
+compare_solutions(30, solve2('test.txt'))
 puts 'Part2', solve2('input.txt')
