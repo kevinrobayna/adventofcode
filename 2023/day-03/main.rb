8380316273ad24f8131eeedde1c1175a47b6bf6f
Kevin Robayna <me@kevinrobayna.com>
2023-12-04 12:20:14 +0000

    solve day3

--- 2023/day-03/main.rb
+++ 2023/day-03/main.rb
@@ -30,8 +30,89 @@ MOVEMENTS = [
 ].freeze
 
 def solve(filename)
-  numbers = []
   board = read_file(filename).lines.map { _1.strip.chars }
+  numbers = possible_parts(board)
+
+  valid_parts = numbers.select do |_number, positions|
+    positions.any? do |inx_x, inx_y|
+      MOVEMENTS.any? do |mov_x, mov_y|
+        next_x = mov_x + inx_x
+        next_y = mov_y + inx_y
+
+        next if next_x.negative?
+        next if next_y.negative?
+        next if next_x >= board.length
+        next if next_y >= board.first.length
+
+        next_v = board[next_x][next_y]
+        next if next_v == '.' || next_v.number?
+
+        true
+      end
+    end
+  end
+
+  valid_parts.map { |number, _| number }.sum
+end
+
+def solve2(filename)
+  board = read_file(filename).lines.map { _1.strip.chars }
+  numbers = possible_parts(board)
+
+  potential_numbers = numbers.select do |_number, positions|
+    positions.any? do |inx_x, inx_y|
+      MOVEMENTS.any? do |mov_x, mov_y|
+        next_x = mov_x + inx_x
+        next_y = mov_y + inx_y
+
+        next if next_x.negative?
+        next if next_y.negative?
+        next if next_x >= board.length
+        next if next_y >= board.first.length
+
+        next_v = board[next_x][next_y]
+        next unless next_v == '*'
+
+        true
+      end
+    end
+  end
+
+  valid_parts = []
+  products = []
+  board.each_with_index do |row, inx_x|
+    row.each_with_index do |v, inx_y|
+      next unless v == '*'
+
+      neighbours = []
+      MOVEMENTS.each do |mov_x, mov_y|
+        next_x = mov_x + inx_x
+        next_y = mov_y + inx_y
+
+        selected = potential_numbers.select do |number, positions|
+          positions.include?([next_x, next_y]) && !neighbours.include?([number, positions])
+        end
+        next if selected.empty?
+        next if selected.length > 1
+
+        neighbours << selected.first
+      end
+
+      next if neighbours.length == 1
+
+      valid_parts << neighbours.first
+      valid_parts << neighbours.last
+      products << neighbours.first.first * neighbours.last.first
+    end
+  end
+
+  debug_board(board, valid_parts)
+
+  products.sum
+end
+
+def possible_parts(board)
+  numbers = []
   board.each_with_index do |row, inx_x|
     number = []
     positions = []
@@ -48,39 +129,14 @@ def solve(filename)
     end
     numbers << [number.join.to_i, positions] unless number.empty?
   end
+  numbers
+end
 
-  puts "Parts To check: #{numbers.map { _1[0] }.join(', ')}"
-  valid_parts = numbers.select do |_number, positions|
-    puts "Checking number: #{_number.to_s.yellow}"
-    x = positions.any? do |inx_x, inx_y|
-      y = MOVEMENTS.any? do |mov_x, mov_y|
-        puts "number: #{_number.to_s.yellow} (#{mov_x},#{mov_y})"
-        next_x = mov_x + inx_x
-        next_y = mov_y + inx_y
-
-        next if next_x.negative?
-        next if next_y.negative?
-        next if next_x >= board.length
-        next if next_y >= board.first.length
-
-        next_v = board[next_x][next_y]
-        next if next_v == '.' || next_v.number?
-
-        true
-      end
-
-      puts "number: #{_number.to_s.green} (#{inx_x},#{inx_y}) is valid" if y
-      puts "number: #{_number.to_s.red} (#{inx_x},#{inx_y}) is not valid" unless y
-      y
-    end
-    puts "number: #{_number.to_s.green} is valid" if x
-    puts "number: #{_number.to_s.red} is not valid" unless x
-    x
-  end
-
+def debug_board(board, valid_parts)
   board.each_with_index do |row, inx_x|
     row.each_with_index do |v, inx_y|
-      board[inx_x][inx_y] = v.red if v != '.' && !v.number?
+      board[inx_x][inx_y] = v.green if v != '.' && v != '*' && !v.number?
+      board[inx_x][inx_y] = v.red if v == '*'
     end
   end
   valid_parts.each do |_number, positions|
@@ -89,18 +145,12 @@ def solve(filename)
     end
   end
   puts board.map(&:join).join("\n")
-  valid_parts.map { |number, _| number }.sum
 end
 
-def solve2(filename)
-  read_file(filename)
-  0
-end
-
-# compare_solutions(4361, solve('test.txt'))
+compare_solutions(4361, solve('test.txt'))
 compare_solutions(4361, solve('test2.txt'))
 compare_solutions(557_705, solve('input.txt'))
 puts 'Part1', solve('input.txt')
 
-compare_solutions(0, solve2('test.txt'))
+compare_solutions(467_835, solve2('test.txt'))
 puts 'Part2', solve2('input.txt')
