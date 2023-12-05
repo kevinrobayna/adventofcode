5a33fbc08917f322d951819d63568d41929968ab
Kevin Robayna <me@kevinrobayna.com>
2023-12-05 08:26:22 +0000

    init day5

---
+++ 2023/day-05/main.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+def compare_solutions(expected, actual)
+  raise "Expected #{expected} but got #{actual}" unless expected == actual
+
+  puts "Congratulations! Got expected result (#{expected})"
+end
+
+def read_file(filename)
+  # Get the directory of the currently executing script
+  # Join the script directory with the filename
+  # Read the content of the file
+  File.read(File.join(__dir__, filename))
+end
+
+def solve(filename)
+  read_file(filename)
+  0
+end
+
+def solve2(filename)
+  read_file(filename)
+  0
+end
+
+compare_solutions(0, solve('test.txt'))
+puts 'Part1', solve('input.txt')
+
+compare_solutions(0, solve2('test.txt'))
+puts 'Part2', solve2('input.txt')
