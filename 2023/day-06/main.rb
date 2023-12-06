26974f93d26750d2258a205c034350a61efca2fa
Kevin Robayna <me@kevinrobayna.com>
2023-12-06 22:13:45 +0000

    solve day 6
    way easier than yesterday's xD

--- 2023/day-06/main.rb
+++ 2023/day-06/main.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry'
 def compare_solutions(expected, actual)
   raise "Expected #{expected} but got #{actual}" unless expected == actual
 
@@ -14,17 +15,32 @@ def read_file(filename)
 end
 
 def solve(filename)
-  read_file(filename)
-  0
+  input = read_file(filename).lines
+  times = input.first.scan(/\d+/).map(&:to_i)
+  distances = input.last.scan(/\d+/).map(&:to_i)
+
+  calculate_ways(times, distances)
 end
 
 def solve2(filename)
-  read_file(filename)
-  0
+  input = read_file(filename).lines
+  times = input.first.scan(/\d+/).join.to_i
+  distances = input.last.scan(/\d+/).join.to_i
+
+  calculate_ways([times], [distances])
 end
 
-compare_solutions(0, solve('test.txt'))
+def calculate_ways(times, distances)
+  times.zip(distances).reduce(1) do |acc, (t, d)|
+    ways = (1...t).filter do |hold|
+      (t - hold) * hold > d
+    end
+    acc * ways.length
+  end
+end
+
+compare_solutions(288, solve('test.txt'))
 puts 'Part1', solve('input.txt')
 
-compare_solutions(0, solve2('test.txt'))
+compare_solutions(71_503, solve2('test.txt'))
 puts 'Part2', solve2('input.txt')
