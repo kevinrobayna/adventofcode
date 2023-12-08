180599e5d25cf0cfb1b687386217a33097d6ca7f
Kevin Robayna <me@kevinrobayna.com>
2023-12-08 10:41:35 +0000

    solve part1 day7

--- 2023/day-07/main.rb
+++ 2023/day-07/main.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry'
 def compare_solutions(expected, actual)
   raise "Expected #{expected} but got #{actual}" unless expected == actual
 
@@ -13,9 +14,33 @@ def read_file(filename)
   File.read(File.join(__dir__, filename))
 end
 
+TYPES = {
+  [5] => 'five_of_kind',
+  [4, 1] => 'four_of_kind',
+  [3, 2] => 'full_house',
+  [3, 1, 1] => 'three_of_kind',
+  [2, 2, 1] => 'two_pair',
+  [2, 1, 1, 1] => 'one_pair',
+  [1, 1, 1, 1, 1] => 'high_card'
+}.freeze
+
 def solve(filename)
-  read_file(filename)
-  0
+  read_file(filename).each_line.map do |line|
+    cards, bid = line.scan(/\w+/)
+    [cards, bid.to_i]
+  end.sort_by do |cards, _bid|
+    ranks = '23456789TJQKA'
+    config = cards.split('').tally
+    type = TYPES[config.values.sort.reverse]
+    type_index = TYPES.values.reverse.index(type)
+    rank = cards.split('').map { |card, _inx| ranks.rindex(card) }
+    order_by = [type_index, rank].flatten
+    # puts "#Cards=#{cards}, type=#{type}, order_by=#{order_by}"
+    order_by
+  end.each_with_index.sum do |((cards, bid), inx)|
+    puts "Cards #{cards} bid: #{bid} on rank: ##{inx + 1}"
+    (inx + 1) * bid
+  end
 end
 
 def solve2(filename)
@@ -23,7 +48,7 @@ def solve2(filename)
   0
 end
 
-compare_solutions(0, solve('test.txt'))
+compare_solutions(6440, solve('test.txt'))
 puts 'Part1', solve('input.txt')
 
 compare_solutions(0, solve2('test.txt'))
