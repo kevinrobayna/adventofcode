27213fd17a6d69b769bc6ea3e465b57f48eb5a4e
Kevin Robayna <me@kevinrobayna.com>
2023-12-11 16:40:27 +0000

    push solution for part1 day10

--- 2023/day-10/main.rb
+++ 2023/day-10/main.rb
@@ -20,15 +20,31 @@ def solve(filename)
   start = []
   grid = read_file(filename).each_line.with_index.map do |line, row|
     chars = line.strip.chars
-    start = [[row, chars.index('S')]] if chars.include?('S')
+    start = [row, chars.index('S')] if chars.include?('S')
     chars
   end
-  queue = [start]
-  until queue.empty?
-    inx_x, inx_y = queue.pop
-    p inx_x, inx_y
-  end
+  start_neighburs = possible_ends(grid, start)
+  p start_neighburs
+  return 0 if start_neighburs.length.zero?
 
+  completed = []
+  queue = [[start]]
+  until queue.empty?
+    path = queue.pop
+    inx_x, inx_y = path.last
+
+    next_paths = valid_movements([inx_x, inx_y], grid, next_positions(grid[inx_x][inx_y]))
+                  .reject { |next_x, next_y| path.include?([next_x, next_y]) }
+
+    completed << path if next_paths.empty? && (start_neighburs & path) == start_neighburs
+
+    next_paths.each { |next_pos| queue << (path.dup << next_pos) }
+
+  end
+  completed.each do |path|
+    puts debug_board(grid, path)
+  end
+  completed.first.length / completed.length
 end
 
 def solve2(filename)
@@ -36,40 +52,71 @@ def solve2(filename)
   0
 end
 
+def possible_ends(grid, start)
+  up = [-1, 0]
+  down = [1, 0]
+  right = [0, 1]
+  left = [0, -1]
+
+  valid_movements(start, grid, [up, down, left, right])
+end
+
 def next_positions(value)
+  up = [-1, 0]
+  down = [1, 0]
+  right = [0, 1]
+  left = [0, -1]
   case value
+  when 'S'
+    [up, down, left, right]
   when '|'
-    [[1, 0], [-1, 0]]
+    [up, down]
   when '-'
-    [[0, 1], [0, 1]]
+    [right, left]
   when 'L'
-    [[0, 1], [0, 1]]
+    [up, right]
   when 'J'
-    [[0, 1], [0, 1]]
+    [up, left]
   when '7'
-    [[0, 1], [0, 1]]
+    [down, left]
   when 'F'
-    [[0, 1], [0, 1]]
+    [down, right]
   else
     []
   end
 end
 
-def debug_board(board)
-  board.map do |row|
-    row.map do |v|
-      if v.nil?
-        'x'.red
-      else
-        v.to_s.green
-      end
-    end
-  end.map(&:join).join("\n")
+def valid_movements(current, grid, movements)
+  movements.map { |inc_x, inc_y| [current.first + inc_x, current.last + inc_y] }
+           .select { |next_x, _next_y| next_x >= 0 && next_x <= grid.length }
+           .select { |next_x, next_y| next_y >= 0 && next_y <= grid[next_x].length }
+           .select do |x, y|
+    next_positions(grid[x][y])
+      .map { |inc_x, inc_y| [x + inc_x, y + inc_y] }
+      .select { |next_x, _next_y| next_x >= 0 && next_x <= grid.length }
+      .select { |next_x, next_y| next_y >= 0 && next_y <= grid[next_x].length }
+      .include?(current)
+  end
 end
 
+def debug_board(board, path)
+  board.each_with_index.map do |row, inx_x|
+    row.each_with_index.map do |v, inx_y|
+      if path.include?([inx_x, inx_y])
+        v.green
+      else
+        v.red
+      end
+    end
+  end.map(&:join).join("\n") + "\n" + "\n"
+end
+
+# Easy ones
 compare_solutions(4, solve('test.txt'))
-compare_solutions(4, solve('test2.txt'))
 compare_solutions(8, solve('test3.txt'))
+
+# Annoying ones
+compare_solutions(4, solve('test2.txt'))
 compare_solutions(8, solve('test4.txt'))
 puts 'Part1', solve('input.txt')
 
