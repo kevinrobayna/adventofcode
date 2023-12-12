d49cb13e4e662e138c13c84a3f75327566be5ced
Kevin Robayna <me@kevinrobayna.com>
2023-12-12 19:16:00 +0000

    day11 part1 and 2

--- 2023/day-11/main.rb
+++ 2023/day-11/main.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'pry'
+require 'colorize'
+
 def compare_solutions(expected, actual)
   raise "Expected #{expected} but got #{actual}" unless expected == actual
 
@@ -13,18 +16,60 @@ def read_file(filename)
   File.read(File.join(__dir__, filename))
 end
 
-def solve(filename)
-  read_file(filename)
-  0
+def solve(filename, difference = 2)
+  small_grid = read_file(filename).each_line.map do |line|
+    line.strip.chars
+  end
+
+  positions = []
+  empty_columns = Set.new
+  empty_rows = Set.new
+
+  small_grid.each_with_index do |row, inx_x|
+    row.each_with_index do |v, inx_y|
+      positions << [inx_x, inx_y] if v == '#'
+      if small_grid.none? { |r| r[inx_y] == '#' } && !row.include?('#')
+        empty_rows << inx_x
+        empty_columns << inx_y
+      end
+    end
+  end
+
+  positions.combination(2).sum do |p1, p2|
+    distance(p1, p2, empty_rows, empty_columns, difference)
+  end
 end
 
-def solve2(filename)
-  read_file(filename)
-  0
+def distance(point1, point2, empty_rows, empty_columns, separtion)
+  x, y = point1
+  dx, dy = point2
+
+  distance = (x - dx).abs + (y - dy).abs
+
+  x_min, x_max = [x, dx].minmax
+  y_min, y_max = [y, dy].minmax
+  distance += (empty_rows.select { _1.between?(x_min, x_max) }.length * (separtion - 1))
+  distance += (empty_columns.select { _1.between?(y_min, y_max) }.length * (separtion - 1))
+
+  distance
 end
 
-compare_solutions(0, solve('test.txt'))
-puts 'Part1', solve('input.txt')
+def debug_board(board)
+  board.each_with_index.map do |row, _inx_x|
+    row.each_with_index.map do |v, inx_y|
+      if v != '.'
+        v.red
+      elsif board.none? { |r| r[inx_y] == '#' } && !row.include?('#')
+        v.green
+      else
+        v.blue
+      end
+    end
+  end.map(&:join).join("\n")
+end
 
-compare_solutions(0, solve2('test.txt'))
-puts 'Part2', solve2('input.txt')
+compare_solutions(374, solve('test.txt', 2))
+compare_solutions(1030, solve('test.txt', 10))
+compare_solutions(8410, solve('test.txt', 100))
+puts 'Part1', solve('input.txt', 2)
+puts 'Part2', solve('input.txt', 1_000_000)
