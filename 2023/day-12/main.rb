f23786ef41a518ac2b2468948e49ab6f82b41efa
Kevin Robayna <me@kevinrobayna.com>
2023-12-13 09:47:53 +0000

    Change solution to generate combinations myself

--- 2023/day-12/main.rb
+++ 2023/day-12/main.rb
@@ -15,58 +15,55 @@ def read_file(filename)
   File.read(File.join(__dir__, filename))
 end
 
-def solve(filename)
+def solve(filename, n_folds = 1)
   read_file(filename).each_line.reduce(0) do |acc, line|
     springs, rules = line.strip.split(' ')
-    springs = springs.chars
-    rules = rules.split(',').map(&:to_i)
 
-    matcher = Regexp.new("(#{springs.join.gsub('.', '\.').gsub('?', '(\?|.|#)')})")
+    # Expand the number of folds
+    # Replace multiple instances of . with a single . since .. == .
+    # Append a . to the end of the input to not need to worry about '# 1' being invalid
+    springs = "#{([springs] * n_folds).join('?')}.".gsub(/\.+/, '.')
+    rules = rules.split(',').map(&:to_i) * n_folds
 
-    posibilities = create_table(springs.length).select do |values|
-      values.join.match(matcher)
-    end
-    posibilities = posibilities.select do |values|
-      found_pound = false
-      count = 0
-      combination = []
-      values.each do |v|
-        if v == '#'
-          count += 1
-          found_pound = true
-        else
-          next if found_pound == false || count.zero?
-
-          combination << count
-          count = 0
-        end
-      end
-      if (found_pound == true) && (count != 0)
-
-        combination << count
-        count = 0
-      end
-      # puts "S:#{values.join}, R: #{rules} C:#{combination}"
-      combination == rules
-    end
-
-    # puts "#Total:#{posibilities.length}\n\n"
-
-    acc + posibilities.length
+    acc + count(springs, rules, 0, {})
   end
 end
 
-def solve2(filename)
-  read_file(filename)
-  0
-end
+def count(springs, rules, group_count = 0, cache = {})
+  key = [springs, rules, group_count]
 
-def create_table(n)
-  %w[# .].repeated_permutation(n).to_a
+  return cache[key] if cache[key]
+
+  return cache[key] = 0 if rules.any? { _1 - group_count > springs.length }
+
+  if rules.empty?
+    return cache[key] = 1 unless springs.include?('#')
+
+    return cache[key] = 0
+  end
+
+  head, *left = springs.chars
+  case [head, group_count]
+  in ['?', _]
+    # Explore solution for either value of ?
+    return cache[key] =
+             count("##{left.join}", rules, group_count, cache) + count(".#{left.join}", rules, group_count, cache)
+  in ['#', _]
+    return cache[key] = count(left.join, rules, group_count + 1, cache)
+  in ['.', ^(rules.first)]
+    # We finished completing a group
+    return cache[key] = count(left.join, rules[1..], 0, cache)
+  in ['.', (1..)]
+    # we found a . but the group count does not match so this combination is invalid
+    return cache[key] = 0
+  in ['.', 0]
+    # Move forward
+    return cache[key] = count(left.join, rules, 0, cache)
+  end
 end
 
 compare_solutions(21, solve('test.txt'))
 puts 'Part1', solve('input.txt')
 
-compare_solutions(0, solve2('test.txt'))
-puts 'Part2', solve2('input.txt')
+compare_solutions(525_152, solve('test.txt', 5))
+puts 'Part2', solve('input.txt', 5)
